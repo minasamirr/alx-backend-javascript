c8297d7fc031b4a45a11a55a87b6ea62deec45f6
minasamirr <mina430q@gmail.com>
2024-06-24 21:18:12 +0300

    task

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue (array, appendString) {
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+  return array;
+}
