8e268eb29e92077657b90ddd24caa7dda6c5b522
minasamirr <mina430q@gmail.com>
2024-06-25 15:42:46 +0000

    task

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,13 @@
+export const uploadPhoto = () => {
+  return {
+    status: 200,
+    body: 'photo-profile-1',
+  };
+};
+
+export const createUser = () => {
+  return {
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  };
+};
