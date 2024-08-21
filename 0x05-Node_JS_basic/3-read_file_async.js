f245a7500227602e68d1aae7ebcb7e31a306d64c
minasamirr <mina430q@gmail.com>
2024-08-21 16:26:08 +0300

    fix indentation

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,32 +1,32 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(new Error('Cannot load the database'));
-            } else {
-                const lines = data.trim().split('\n').slice(1);
-                const students = lines.filter(line => line).map(line => line.split(','));
+	return new Promise((resolve, reject) => {
+		fs.readFile(path, 'utf8', (err, data) => {
+			if (err) {
+				reject(new Error('Cannot load the database'));
+			} else {
+				const lines = data.trim().split('\n').slice(1);
+				const students = lines.filter(line => line).map(line => line.split(','));
 
-                console.log(`Number of students: ${students.length}`);
+				console.log(`Number of students: ${students.length}`);
 
-                const fields = {};
-                students.forEach(([firstName, , , field]) => {
-                    if (!fields[field]) {
-                        fields[field] = [];
-                    }
-                    fields[field].push(firstName);
-                });
+				const fields = {};
+				students.forEach(([firstName, , , field]) => {
+					if (!fields[field]) {
+						fields[field] = [];
+					}
+					fields[field].push(firstName);
+				});
 
-                for (const [field, students] of Object.entries(fields)) {
-                    console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-                }
+				for (const [field, students] of Object.entries(fields)) {
+					console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+				}
 
-                resolve();
-            }
-        });
-    });
+				resolve();
+			}
+		});
+	});
 }
 
 module.exports = countStudents;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,22 +4,22 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+	res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    countStudents(process.argv[2])
-        .then(() => {
-            res.write('This is the list of our students\n');
-            res.end();
-        })
-        .catch(err => {
-            res.send(err.message);
-        });
+	countStudents(process.argv[2])
+		.then(() => {
+			res.write('This is the list of our students\n');
+			res.end();
+		})
+		.catch(err => {
+			res.send(err.message);
+		});
 });
 
 app.listen(1245, () => {
-    console.log('Server is running on port 1245');
+	console.log('Server is running on port 1245');
 });
 
 module.exports = app;
